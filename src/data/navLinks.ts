e54bef483e0fbfaa9036793fed5b9c46e51b56ca
wublinux <wublinux@users.noreply.github.com>
2026-07-11 15:41:40 +0800

    Make Projects the primary links view

--- src/data/navLinks.ts
+++ src/data/navLinks.ts
@@ -2,6 +2,6 @@ export const getNavLinks = (base: string) => [
 	{ href: `${base}`, label: 'Home' },
 	{ href: `${base}blog/`, label: 'Blog' },
 	{ href: `${base}tags/`, label: 'Tags' },
-	{ href: `${base}links/`, label: 'Links' },
+	{ href: `${base}links/`, label: 'Projects' },
 	{ href: `${base}about/`, label: 'About' },
 ];
